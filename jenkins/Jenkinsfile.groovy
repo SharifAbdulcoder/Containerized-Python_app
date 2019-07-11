0d1ce5e69760e920ebea91fdba5aea56f9d84f85
SharifAbdulCoder <abdugofir@gmail.com>
2019-07-11 15:21:37 -0500

    Docker_pipeline

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -3,13 +3,13 @@ node('master') {
   }
 
    stage("Docker Build") {
-     dir("${WORKSPACE}/docker-python") {
+     dir("${WORKSPACE}/Docker_pipeline") {
        sh "docker build -t sharifabdulcoder/app . "
      }
    }
 
    stage('Terraform Apply/Plan') {
-           dir("${WORKSPACE}/docker-python") {
+           dir("${WORKSPACE}/Docker_pipeline") {
              echo "##### Pushing image to Amazon ECR ####"
              sh "docker tag http-server:latest 608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server:latest"
              sh "docker push 608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server:latest"
@@ -17,7 +17,7 @@ node('master') {
          }
 
   stage('Terraform Destoy') {
-         dir("${WORKSPACE}/artemis_tf") {
+         dir("${WORKSPACE}/Docker_pipeline") {
            echo "##### Terraform Destroying the Changes ####"
            sh "terraform destroy  --auto-approve"
          }
