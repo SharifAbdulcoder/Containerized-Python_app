c7cf81d66f4750b7e094a7a9c52ffa486d30cfff
SharifAbdulCoder <abdugofir@gmail.com>
2019-07-11 16:07:50 -0500

    a

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -2,23 +2,8 @@ node('master') {
     git 'https://github.com/SharifAbdulcoder/Docker-python.git'
   }
 
-   stage("Docker Build") {
-     dir("${WORKSPACE}/Docker_pipeline/") {
-       sh "docker build -t sharifabdulcoder/app . "
-     }
-   }
+  stage('Build an image') {
+    sample = docker.build("608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server")
+  }
 
-   stage('Push') {
-           dir("${WORKSPACE}/Docker_pipeline") {
-             echo "##### Pushing image to Amazon ECR ####"
-             sh "docker tag http-server:latest 608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server:latest"
-             sh "docker push 608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server:latest"
-           }
-         }
-
-  // stage('Terraform Destoy') {
-  //        dir("${WORKSPACE}/Docker_pipeline") {
-  //          echo "##### Terraform Destroying the Changes ####"
-  //          sh "terraform destroy  --auto-approve"
-  //        }
-  //      }
+  withCredentials([])
