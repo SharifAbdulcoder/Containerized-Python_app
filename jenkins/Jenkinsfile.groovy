e31920367b0a76e9872633670e53dcf3407bd24e
SharifAbdulCoder <abdugofir@gmail.com>
2019-07-11 18:19:10 -0500

    ;_

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -1,9 +1,17 @@
 node('master') {
-    git 'https://github.com/SharifAbdulcoder/Docker-python.git'
+  git 'https://github.com/SharifAbdulcoder/Docker-python.git'
   }
 
-  stage('Build an image') {
-    sample = docker.build("608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server")
-  }
 
-  withCredentials([])
+  stage('Build Docker') {
+  if (!fileExists("Docker-python/Dockerfile")) {
+         error('Dockerfile missing.')
+       }
+    }
+
+
+  stage('Build Docker') {
+  dir("${WORKSPACE}/Docker-python") {
+      sh "docker build -t SharifAbdulcoder/app"
+      }
+    }
