6b5d4af578d78989930faf297c9decc2d43e11ea
SharifAbdulCoder <abdugofir@gmail.com>
2019-07-11 20:16:20 -0500

    updated jenkins

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -1,23 +1,56 @@
 node('master') {
-  git 'https://github.com/SharifAbdulcoder/Docker-python.git'
+    git 'https://github.com/SharifAbdulcoder/Docker-python.git'
   }
+   stage("Docker build") {
+     dir("${WORKSPACE}/dp") {
+       sh "echo 'hello' "
+     }
+   }
 
-
-  stage('Build Docker') {
-  if (!fileExists("Docker-python/Dockerfile")) {
-         error('Dockerfile missing.')
-       }
-    }
-
-
-  stage('Build Docker') {
-    dir("${WORKSPACE}/Docker-python") {
-      sh "docker build -t SharifAbdulcoder/app"
+   stage('Terraform Apply/Plan') {
+           dir("${WORKSPACE}/artemis_tf") {
+             echo "##### Terraform Applying the Changes ####"
+             sh "terraform apply  --auto-approve"
+           }
+           dir("${WORKSPACE}/artemis_tf/") {
+             sh "terraform plan"
       }
     }
 
-   stage('Build Docker') {
-     dir("${WORKSPACE}/Docker-python") {
-       sh "docker run -dti -p 80:8080 SharifAbdulcoder/app"
-     }
-  }
+    stage('Terraform Destoy') {
+           dir("${WORKSPACE}/artemis_tf") {
+             sh "terraform destroy  --auto-approve"
+           }
+         }
+       }
+
+
+}
+
+
+
+//
+//
+// node('master') {
+//   git 'https://github.com/SharifAbdulcoder/Docker-python.git'
+//   }
+//
+//
+//   stage('Build Docker') {
+//   if (!fileExists("Docker-python/Dockerfile")) {
+//          error('Dockerfile missing.')
+//        }
+//     }
+//
+//
+//   stage('Build Docker') {
+//     dir("${WORKSPACE}/Docker-python") {
+//       sh "docker build -t SharifAbdulcoder/app"
+//       }
+//     }
+//
+//    stage('Build Docker') {
+//      dir("${WORKSPACE}/Docker-python") {
+//        sh "docker run -dti -p 80:8080 SharifAbdulcoder/app"
+//      }
+//   }
