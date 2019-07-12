1bca52503ae964128ee57a5cd929b6fa53d41006
SharifAbdulCoder <abdugofir@gmail.com>
2019-07-11 20:25:28 -0500

    ECR

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -4,27 +4,27 @@ node('master') {
   }
    stage("Docker build") {
      dir("${WORKSPACE}/dp") {
-       sh "echo 'hello' "
+       sh "docker build -t SharifAbdulcoder/app ."
      }
    }
 
-   stage('Terraform Apply/Plan') {
-           dir("${WORKSPACE}/artemis_tf") {
-             echo "##### Terraform Applying the Changes ####"
-             sh "terraform apply  --auto-approve"
+   stage('Docker push') {
+           dir("${WORKSPACE}/dp") {
+             sh "docker build -t http-server ."
            }
-           dir("${WORKSPACE}/artemis_tf/") {
-             sh "terraform plan"
+           dir("${WORKSPACE}/dp/") {
+             sh "docker tag http-server:latest 608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server:latest"
       }
-    }
+           dir("${WORKSPACE}/dp/") {
+             sh "docker push 608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server:latest"
+     }
+  }
 
     stage('Terraform Destoy') {
-           dir("${WORKSPACE}/artemis_tf") {
-             sh "terraform destroy  --auto-approve"
+           dir("${WORKSPACE}/dp") {
+             sh "docker run -dti -p 80:8080 608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server:latest"
            }
          }
-
-
 }
 
 
